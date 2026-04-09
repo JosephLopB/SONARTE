56681db8bbdbd2fb8ae6d6ca2172f58197a3867c
Joseph <joshyngppo@gmail.com>
2026-04-09 13:23:58 -0500

    Fixed

--- SONARTE/src/components/Header.jsx
+++ SONARTE/src/components/Header.jsx
@@ -2,6 +2,27 @@
 
 export const Header = () => {
   return (
-    <>Header</>
+    <>
+    
+    <div className="container">
+      <div className="navbar">
+
+        <div className="logo text-primary">
+          logo<img src="/assets/logo.png" alt="Logo"/>
+        </div>
+        
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        
+        </div>
+
+    </div>
+
+    </>
   )
 }
