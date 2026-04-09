83c9a5bebfd6c18c91f8c7d2e8799d71a62b01fe
Joseph <joshyngppo@gmail.com>
2026-04-09 14:43:36 -0500

    Imgs y footer

--- SONARTE/src/components/Footer.jsx
+++ SONARTE/src/components/Footer.jsx
@@ -1,6 +1,29 @@
 
 export const Footer = () => {
   return (
-    <>Footer</>
+    <>
+
+    <div className="container">
+      
+      <footer>
+
+        <div>
+          <h1> SonArte</h1>
+          <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore libero iusto commodi odit architecto possimus earum porro.</p>
+        </div>
+
+        <div className="">
+          <div className="">Siguenos y se parte de la comunidad SONARTE</div>
+          <div className="">
+
+          </div>
+
+
+          <div className=""></div>
+        </div>
+      </footer>
+    </div>
+
+    </>
   )
 }
